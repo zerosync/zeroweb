3b725d20b9ac4039b91f59497ee7c4226c5277f1
BurneBurn <mail@bernhard-finger.de>
2014-02-24 17:22:39 +0100

    finished functionality

--- src/ZS_server.c
+++ src/ZS_server.c
@@ -11,7 +11,13 @@ bool fileIncome = false;
 uint32_t size_count_total = 0;
 uint32_t msg_size_total = 0;
 char *fName;
+char *zsPath;
 
+/**
+  *
+  *  This function checks, if the connection is established correctly. If true, the connection is finally established.
+  *
+  **/
 nopoll_bool on_connection_opened (noPollCtx * ctx, noPollConn * conn, noPollPtr user_data)
 {
     if (! nopoll_conn_set_sock_block (nopoll_conn_socket (conn), nopoll_false)) {
@@ -33,7 +39,14 @@ nopoll_bool on_connection_opened (noPollCtx * ctx, noPollConn * conn, noPollPtr 
     return nopoll_true;
 }
 
-
+void deleteMsg (char *path_to_file) 
+{
+    if (remove(path_to_file) < 0) {
+        printf("ERROR: An error occured, while deleting the file %s \n", path_to_file);
+        return;
+    }
+}
+ 
 void handleMsg (noPollCtx *ctx, noPollConn *con, noPollMsg *msg, noPollPtr *user_data)
 {
     unsigned long msg_size = nopoll_msg_get_payload_size(msg);
@@ -41,16 +54,32 @@ void handleMsg (noPollCtx *ctx, noPollConn *con, noPollMsg *msg, noPollPtr *user
     clientMsg = (uint8_t *) nopoll_msg_get_payload(msg);
  
     if (!fileIncome) {
-        printf("Start Uploading...\n");
         printf("Server received (size: %d)\n", nopoll_msg_get_payload_size(msg));
-        fileIncome = true;
         /* Check if the msg is the initializing CMD msg before the file msgs*/
         char tmp[CMD_SIZE];
         fName = malloc((msg_size-CMD_SIZE)*sizeof(char));
         strncpy (tmp, clientMsg, 3);
         tmp[3] = '\0';
         
+        printf("Server received msg: %s \n", (char *)clientMsg);
         if (strcmp(tmp, "ZSF") == 0) { // if the cmd prefix is set in the msg, the following chars are the Name of the incoming file
+            if (strncmp(clientMsg, "ZSFDEL", 6) == 0) {
+                char *fName_toDelete = malloc(((msg_size-(CMD_SIZE +3))* sizeof(char)));
+                printf("Delete triggered!\n");
+                
+                int i;
+                for (i = 0; i < msg_size; i++){
+                    fName_toDelete[i] = clientMsg[i+(CMD_SIZE-1+3)]; // fill the filePath in the fName_toDelete string
+                }
+                fName_toDelete[strlen(fName_toDelete)] = '\0';
+              
+                printf("deleting...%s \n", fName_toDelete); 
+                deleteMsg(fName_toDelete); 
+                free(fName_toDelete);
+                return;
+            }
+            fileIncome = true;
+            
             int y;
             // get uint32 from network byte order
             msg_size_total = ((uint32_t) (clientMsg [3]) << 24)
@@ -67,7 +96,7 @@ void handleMsg (noPollCtx *ctx, noPollConn *con, noPollMsg *msg, noPollPtr *user
     } else {
         printf("Server received (size: %d)\n", nopoll_msg_get_payload_size(msg));
         printf("msg_size_total: %"PRIu32" size_count_total: %"PRIu32"\n" , msg_size_total, size_count_total);
-        char *path = "/home/burne/Documents/bigFiles/testDestination/";
+        char *path = zsPath;
         char *fullPath;
         fullPath = malloc((strlen(fName)+strlen(path) + 1)*sizeof(char));
         strcpy(fullPath, path);
@@ -103,8 +132,16 @@ void handleMsg (noPollCtx *ctx, noPollConn *con, noPollMsg *msg, noPollPtr *user
 
 int main (int argc, char **argv) 
 {
+    if (argc != 2) {
+        printf("Sorry, the Server has to have at least 1 Argument! Retry please! \n");
+        return EXIT_FAILURE;
+    }
+
     printf("Server started!\n");
-    
+    printf("Argv[1] %s \n", argv[1]);
+    zsPath = argv[1];
+    strcat(zsPath, "/");
+
     noPollCtx *ctx = nopoll_ctx_new();
     if (!ctx){
         printf("Could not create context. Shutting down!\n");
